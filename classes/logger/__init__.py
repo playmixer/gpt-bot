f5a1fc71601b72ff9a890710bcfae729f5755a28
shkirya.ns <shkirya.ns@oilpc.ru>
2023-11-07 10:54:53 +0600

    detail logs

--- classes/logger/__init__.py
+++ classes/logger/__init__.py
@@ -7,5 +7,5 @@ def logger(prefix='', suffix=''):
     if not os.path.exists("./logs"):
         os.mkdir("./logs")
     logging.basicConfig(filename=f'./logs/{prefix}{datetime.now().strftime("%Y-%m-%d")}{suffix}.log', encoding='utf-8',
-                        level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+                        level=logging.INFO, format='%(name)s %(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     return logging
